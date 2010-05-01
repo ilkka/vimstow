1ee2fa934b2fbb53f3a219bc3f861ec51dbd447f
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2010-05-01 16:04:23 +0300

    Outline some specs

--- spec/vimstow_spec.rb
+++ spec/vimstow_spec.rb
@@ -1,7 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/helper.rb')
 
 describe "Vimstow" do
-  it "should have some specs" do
-    pending "write the damn specs"
+  it "should output its version with -v" do
+    pending "write test"
   end
+
+  context "when stowing" do
+    it "should symlink top-level dirs when they don't exist" do
+      pending "write test"
+    end
+    it "should symlink top-level dir contents when top-level dirs exist" do
+      pending "write test"
+    end
+    it "should symlink nested dir contents if nested dirs exist" do
+      pending "write test"
+    end
+    it "should convert symlinks to dirs when a nested target dir is a symlink" do
+      pending "write test"
+    end
+  end
+
+  context "when unstowing"
 end
