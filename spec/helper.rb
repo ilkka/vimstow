b51ba1f28d5915a82c9ae0cb318fb2b46afc4408
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2010-05-01 18:00:44 +0300

    Add a helper method on Kernel that captures stdout.

--- spec/helper.rb
+++ spec/helper.rb
@@ -2,3 +2,17 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/vimstow.rb')
 
 require 'spec'
 require 'construct'
+
+# redirect stdout
+require 'stringio'
+module Kernel
+  def capture_stdout
+    out = StringIO.new
+    $stdout = out
+    yield
+    return out.string
+  ensure
+    $stdout = STDOUT
+  end
+end
+
